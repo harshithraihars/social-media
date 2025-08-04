e8cbcb18405c3811e9daf4004de9eb67ce753193
harshithraihars <harshithraiharsu@gmail.com>
2025-08-04 18:46:43 +0530

    ui conflict resolved

--- app/Mentorship/page.tsx
+++ app/Mentorship/page.tsx
@@ -2,14 +2,28 @@ import dynamic from "next/dynamic";
 import Loader from "../Mentor/loading";
 
 // Lazy load components
-const Mentee = dynamic(() => import("./pages/Mentee"), { ssr: false, loading: () =><div><Loader/></div> });
-const MentorShipHeader = dynamic(() => import("./pages/MentorShipHeader"), { ssr: false, loading: () => <div><Loader/></div> });
+const Mentee = dynamic(() => import("./pages/Mentee"), {
+  ssr: false,
+  loading: () => (
+    <div>
+      <Loader />
+    </div>
+  ),
+});
+const MentorShipHeader = dynamic(() => import("./pages/MentorShipHeader"), {
+  ssr: false,
+  loading: () => (
+    <div>
+      <Loader />
+    </div>
+  ),
+});
 
 const MentorshipPage = () => {
   return (
     <div className="min-h-screen shadow-2xl mt-14 transition-all duration-500 bg-gradient-to-br from-[#eef5ff] via-[#dbeafe] to-[#bfdbfe]">
       {/* Wrapper to maintain content width */}
-      <div className="mx-0 md:mx-10 px-4 sm:px-6 lg:px-8">
+      <div className="mx-0 md:mx-10 px-4 sm:px-6 lg:px-8 flex-grow">
         {/* Header Section */}
         <MentorShipHeader />
         <Mentee />
